d82eae78d25561992c67409f38808f02b7dce609
Jasur <jasur0105@gmail.com>
2020-12-05 00:22:33 +0500

    доработка

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let x = []
-for (;;) {
-    let y = prompt("Введите команду");
+while (true) {
+    let y = prompt("Введите команду (stop, add, del)");
     if (y == "stop") {
         break
     }
     let d = y.split(", ")
     if (d[0] == "del") {
-        x.splice(d[1], 1)
-    } else if (d[0] == "add"){
+        for (let i = 0; i < x.length; i++) {
+            if (x[i] == d[1]) {
+                x.splice(i, 1);
+            }
+        }
+    } else if (d[0] == "add") {
         x.push(d[1])
-    }  else if (y == "alert(arr)") {
-        x.forEach(function (array){
-            alert(array)
-        })
     }
-    
+    alert(x)
+
 }
 console.log(x);
--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 function vowels() {
     var x = 0;
-    var stringsearch = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+    var stringsearch = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я", "А", "О", "И", "Е", "Ё", "Э", "Ы", "У", "Ю", "Я"];
     var str = prompt("Введите текст:");
     for (var i = 0; i < str.length; i++)
         for (var j = 0; j < stringsearch.length; j++)
